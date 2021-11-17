ee76a9e238c2f7361a22999dfbe1dc82f1d1f0c8
anirudha-chari <anirudha_chari@persistent.com>
2021-11-17 17:06:27 +0530

    fix image post issues

--- src/components/UI/AdminAddProduct.jsx
+++ src/components/UI/AdminAddProduct.jsx
@@ -1,12 +1,5 @@
-import { Grid, makeStyles } from "@material-ui/core";
-import { TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import AdminSidebar from "./AdminSidebar";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import Button from '@mui/material/Button';
 import "../styles/adminpage.css"
 import "../styles/adminaddproduct.css";
 import 'bootstrap'
@@ -14,9 +7,10 @@ import 'bootstrap'
 function AdminAddProduct() {
     var formData = new FormData()
     const url = "https://6194599f9b1e780017ca1f28.mockapi.io/addproduct"
+    let rs, paise
 
     const handleTitleChange = e => {
-        formData.append( "name", e.target.value )
+        formData.append("name", e.target.value)
         console.log(e.target.value)
     }
     const handleCategoryChange = e => {
@@ -24,7 +18,7 @@ function AdminAddProduct() {
         console.log(e.target.value)
     }
     const handleStockChange = e => {
-        formData.append("stock",e.target.value)
+        formData.append("stock", e.target.value)
         console.log(e.target.value)
     }
     const handleDescChange = e => {
@@ -36,16 +30,20 @@ function AdminAddProduct() {
         console.log(e.target.value)
     }
     const handlePriceRsChange = e => {
-        formData.append("price", e.target.value)
+        rs = e.target.value
         console.log(e.target.value)
     }
     const handlePricePaiseChange = e => {
-        formData.append("price", e.target.value)
+        paise = e.target.value
         console.log(e.target.value)
     }
 
     function handleSubmit(e) {
         e.preventDefault();
+        const price = Number(rs+ '.' +paise)
+        console.log(price)
+        formData.append('price', price)
+
         fetch(url, {
             method: 'POST',
             headers: {
@@ -65,14 +63,14 @@ function AdminAddProduct() {
         <div className="container row mt-2" >
             <AdminSidebar />
             <div className="col-7 container">
-                <form >
+                <form onSubmit={e => handleSubmit(e)}>
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" id="title" aria-describedby="emailHelp" onChange={(e)=>handleTitleChange(e)} />
+                        <input type="text" className="form-control" id="title" aria-describedby="emailHelp" onChange={(e) => handleTitleChange(e)} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="scategory" className="form-label">Category</label>
-                        <select className="form-select" aria-label="category" defaultValue={null} id="category" onChange={(e)=>handleCategoryChange(e)}>
+                        <select className="form-select" aria-label="category" defaultValue={null} id="category" onChange={(e) => handleCategoryChange(e)}>
                             <option value="1">One</option>
                             <option value="2">Two</option>
                             <option value="3">Three</option>
@@ -80,19 +78,20 @@ function AdminAddProduct() {
                     </div>
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label">Description</label>
-                        <textarea className="form-control" id="description" rows="3" onChange={(e)=>handleDescChange(e)}></textarea>
+                        <textarea className="form-control" id="description" rows="3" onChange={(e) => handleDescChange(e)}></textarea>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="formFile" className="form-label">Upload Image</label>
-                        <input className="form-control" type="file" id="formFile" onChange={(e)=>handleFileChange(e)}/>
+                        <input className="form-control" type="file" id="formFile" onChange={(e) => handleFileChange(e)} />
                     </div>
-                    <div className="mb-3 col-3">
+                    <div className="mb-3">
                         <label htmlFor="stock" className="form-label">Stock</label>
-                        <input type="number" className="form-control" id="stock" onChange={(e)=>handleStockChange(e)} />
+                        <input type="number" className="form-control" id="stock" onChange={(e) => handleStockChange(e)} />
                     </div>
-                    <div className="mb-3 col-3">
-                        <label htmlFor="stock" className="form-label">price</label>
-                        <input type="number" className="form-control" id="stock" onChange={(e)=>handlePriceRsChange(e)} />
+                    <div className="input-group mb-3">
+                        <span className="input-group-text">Rs</span>
+                        <input type="number" className="form-control" aria-label="Price" onChange={e => handlePriceRsChange(e)} /> <span className="input-group-text">.</span>
+                        <input type="number" className="form-control" onChange={e => handlePricePaiseChange(e)}/>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
