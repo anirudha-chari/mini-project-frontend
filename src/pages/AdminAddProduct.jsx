701981618c64a3d98e4c2e1ab0cb675d5a6688d1
dhanshreerane <dhanshree_rane@persistent.com>
2021-11-18 16:31:31 +0530

    Changes to Add Product Page
    Changes to Add Product Page

--- src/pages/AdminAddProduct.jsx
+++ src/pages/AdminAddProduct.jsx
@@ -1,95 +1,116 @@
-import React, { useRef, useState } from "react";
+import React, { Component } from 'react'
 import AdminSidebar from "../components/UI/AdminSidebar";
-import "../styles/adminpage.css"
-import "../styles/adminaddproduct.css";
-import 'bootstrap'
+import ProductService from "../ProductService"
+export class AdminAddProduct extends Component {
 
-function AdminAddProduct() {
-    const url = "https://6194599f9b1e780017ca1f28.mockapi.io/addproduct"
-    const titleRef = useRef()
-    const categoryRef = useRef()
-    const stockRef = useRef()
-    const descriptionRef = useRef()
-    const fileRef = useRef()
-    const priceRef = useRef()
-    const [error, setError] = useState()
-
-    var formData = new FormData()
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        formData.append('name', titleRef.current.value)
-        formData.append('category', categoryRef.current.value)
-        formData.append('stock', stockRef.current.value)
-        formData.append('description', descriptionRef.current.value)
-        formData.append('product_photo', fileRef.current.value)
-        formData.append('price', priceRef.current.value)
-
-        for (const iterator of formData.entries()) {
-            console.log(iterator)
+    constructor(props){
+        super(props)
+        this.state={
+            name:"",
+            category:"",
+            price:"",
+            stock:"",
+            description:"",
+            image:""
         }
-
-        fetch(url, {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-            body: formData
-        })
-            .then(res => {
-                !(res.statusText === 'ok') && setError('something went wrong')
-                return res
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+        this.changeNameHandler = this.changeNameHandler.bind(this);
+        this.changeCategoryHandler = this.changeCategoryHandler.bind(this);
+        this.changePriceHandler = this.changePriceHandler.bind(this);
+        this.changeStockHandler = this.changeStockHandler.bind(this);
+        this.changeDescriptionHandler = this.changeDescriptionHandler.bind(this);
+        this.saveProduct = this.saveProduct.bind(this);
+        this.changeImageHandler = this.changeImageHandler.bind(this);
     }
-    return (
-        <div className="container row mt-2" >
-            <AdminSidebar />
-            <div className="col-7 container">
-                {error && <div className="alert alert-danger" role="alert">{error}</div>}
-                <form onSubmit={e => handleSubmit(e)}>
-                    <div className="mb-3">
-                        <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" id="title" aria-describedby="emailHelp" ref={titleRef} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="category" className="form-label">Category</label>
-                        <select className="form-select" aria-label="category" defaultValue={null} id="category" ref={categoryRef}>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="description" className="form-label">Description</label>
-                        <textarea className="form-control" id="description" rows="3" ref={descriptionRef}></textarea>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="formFile" className="form-label">Upload Image</label>
-                        <input className="form-control" type="file" id="formFile" ref={fileRef} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="stock" className="form-label">Stock</label>
-                        <input type="number" className="form-control" id="stock" ref={stockRef} />
-                    </div>
-                    <div className="input-group mb-3">
-                        <span className="input-group-text">Rs</span>
-                        <input type="text" className="form-control" aria-label="Price" ref={priceRef} />
-                        {/* <input type="number" className="form-control" ref={pricePaiseRef} /> */}
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
 
+    saveProduct = (e) => {
+        e.preventDefault();
+        let product1 = {name: this.state.name, category: this.state.category, price: this.state.price,stock:this.state.stock,description:this.state.description};
+        var string_product = JSON.stringify(product1);
+        const formData = new FormData();
+        formData.append("product",string_product);
+        formData.append("file",this.state.image);
+        ProductService.postProduct(formData).then((res) => {
+            console.log(res)
+        })
+    }
+
+    changeNameHandler = (e) => {
+        this.setState({name : e.target.value});
+    }
+    changeCategoryHandler = (e) => {
+        this.setState({category : e.target.value});
+    }
+    changePriceHandler = (e) => {
+        this.setState({price : e.target.value});
+    }
+    changeStockHandler = (e) => {
+        this.setState({stock : e.target.value});
+    }
+    changeDescriptionHandler = (e) => {
+        this.setState({description : e.target.value});
+    }
+    changeImageHandler= (e) =>{
+        this.setState({image:e.target.files[0]})
+    }
+    render() {
+        return (
+            <div>
+                 <AdminSidebar />
+                <div className="container">
+                    <div className="row">
+                        <div className="card col-md-6 offset-md-3 offset-md-3">
+                            <h3>Add Product</h3>
+                            <div className="card-body">
+                                <form >
+                                    <div className="form-group">
+                                        <label>Name</label>
+                                        <input placeholder="Product Name" name="name" className="form-control"
+                                        value={this.state.name} onChange={this.changeNameHandler} required/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Category</label>
+                                        {/* <input placeholder="Category" name="category" className="form-control"
+                                        value={this.state.category} onChange={this.changeCategoryHandler}/>
+                             */}
+                                   <select className="form-select" aria-label="category" defaultValue={null}  value={this.state.category} id="category" onChange={this.changeCategoryHandler} required>
+                                       <option value="1">One</option>
+                                       <option value="2">Two</option>
+                                       <option value="3">Three</option>
+                                       <option value="3">Four</option>
+                                       <option value="3">Five</option>
+                                    </select>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Price</label>
+                                        <input type="number"  placeholder="Price" name="price" className="form-control"
+                                        value={this.state.price} onChange={this.changePriceHandler}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Stock</label>
+                                        <input type="number" placeholder="Stock" name="stock" className="form-control"
+                                        value={this.state.stock} onChange={this.changeStockHandler} required/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Description</label>
+                                        <input placeholder="Product Description" name="description" className="form-control"
+                                        value={this.state.description} onChange={this.changeDescriptionHandler}/>
+                                        <br/>
+                                    </div>
+                                    <div>
+                                    <input type="file" onChange={this.changeImageHandler}/>
+                                    </div>
+                                    <br/>
+                                    <button  className="btn btn-success" onClick={this.saveProduct}>Add Product</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+                
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default AdminAddProduct
