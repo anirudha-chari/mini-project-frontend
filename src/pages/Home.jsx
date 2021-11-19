3c380643591e357098040e45ed3e6123bc47a50b
anirudha-chari <anirudha_chari@persistent.com>
2021-11-19 16:39:31 +0530

    axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,28 +2,37 @@ import { useState, useEffect } from "react"
 import { CardContainer, CategoryCard, ProductCard } from "../components/UI/Card"
 import { BASE_URL } from '../constants/URL'
 import { Link } from "react-router-dom"
+import axios from "axios"
 import 'bootstrap'
 
 export const HomePane = props => {
     const [categories, setCategories] = useState(null)
     const [products, setProducts] = useState(null)
+    const [loadingCategories, setLoadingCategories] = useState()
+    const [loadingProducts, setLoadingProducts] = useState()
     useEffect(() => fetch(BASE_URL + '/products/categories')
         .then(res => res.json())
         .then(json => setCategories(json)), [])
 
     useEffect(() => {
-        fetch(BASE_URL + '/products/')
-            .then(res => res.json())
-            .then(json => setProducts(json.slice(0, 10)))
+        axios.get(BASE_URL + '/products/')
+            .then(setLoadingCategories(true))
+            .then(json => setProducts(json.data))
+            .then(setLoadingCategories(false))
+
+        axios.get(BASE_URL + '/products/categories/')
+            .then(setLoadingProducts(true))
+            .then(json => setCategories(json.data))
+            .then(setLoadingProducts(false))
     }, [])
 
     return (
         <section className="container-fluid">
             <CardContainer title="Shop by health conditions">
-                {categories && categories.map(category => <CategoryCard title={category} key={category} />)}
+                {!loadingCategories && categories && categories.map(category => <CategoryCard title={category} key={category} />)}
             </CardContainer>
             <CardContainer title="Best sellers">
-                {products && products.map(product => {
+                {!loadingProducts && products && products.map(product => {
                     return <ProductCard product={product} key={product.id} />
                 })}
 
